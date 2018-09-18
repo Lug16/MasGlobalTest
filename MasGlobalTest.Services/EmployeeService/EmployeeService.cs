a5b2836970023c6105cfd8253eb27b519cccdf7e
Lug Velez Schmitz <lvelez@arrivelogistics.com>
2018-09-18 17:35:09 -0500

    Adding Ninject

--- MasGlobalTest.Services/EmployeeService/EmployeeService.cs
+++ MasGlobalTest.Services/EmployeeService/EmployeeService.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MasGlobalTest.Data.Dto;
 using MasGlobalTest.Data.Model;
 using MasGlobalTest.Tools.Configuration;
 using MasGlobalTest.Tools.Connectivity;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace MasGlobalTest.Services.EmployeeService
 {
@@ -24,6 +22,8 @@ namespace MasGlobalTest.Services.EmployeeService
         public async Task<IEnumerable<EmployeeDto>> GetEmployeesAsync()
         {
             var response = await apiService.Invoke<Employee>(HttpMethod.Get, configHandler.GetConfigValue<string>("MasGlobalTestApiEmployeesUrl"));
+
+            return null;
         }
     }
 }
--- MasGlobalTest.Services/EmployeeService/IEmployeeService.cs
+++ MasGlobalTest.Services/EmployeeService/IEmployeeService.cs
@@ -1,14 +1,11 @@
-﻿using MasGlobalTest.Data.Model;
-using System;
+﻿using MasGlobalTest.Data.Dto;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MasGlobalTest.Services.EmployeeService
 {
     public interface IEmployeeService
     {
-        Task<IEnumerable<Employee>> GetEmployeesAsync();
+        Task<IEnumerable<EmployeeDto>> GetEmployeesAsync();
     }
 }
